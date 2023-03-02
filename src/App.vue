da4299f9de7d2b6a57bedb3cc0aa315098be4617
Robert Servado <servadorobert@gmail.com>
2023-03-02 16:57:14 -0500

    Modified App.vue and added a few references and comments about the stuff we are importing to our app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue'
+
+// ref handles our state
+// onMounted used for rendering our page / component. It will allow us to create a localStorage
+// computed used to make things in-order and create an array
+// watch, it will watch our references and update our local storage
+
+// references
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
 </script>
 
 <template>
